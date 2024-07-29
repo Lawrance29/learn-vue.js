e382a62db4143ca8df5a4f653711133f7ccf8df2
gloriaza <gloriazalawrance@gmail.com>
2024-07-29 23:37:36 +0700

    learn if-else & v-bind

---
+++ V2/app.js
@@ -0,0 +1,10 @@
+const app = new Vue ({
+    el:'#app',
+    data:{
+        tampilkan: true,
+        pesan1: 'Selamat datang!',
+        pesan2: 'Selamat tinggal!',
+        gambar: 'image.jpg',
+        link:'https://www.google.com',
+    }
+})
