27bfd941478370ac3131e1153cc7c43c474c0ad2
gloriaza <gloriazalawrance@gmail.com>
2024-07-30 22:52:37 +0700

    learn computed & method property

--- V2/app.js
+++ V2/app.js
@@ -6,5 +6,34 @@ const app = new Vue ({
         pesan2: 'Selamat tinggal!',
         gambar: 'image.jpg',
         link:'https://www.google.com',
-    }
+        warna: 'hijau',
+        stylingLink : 'text-decoration: none; color: chocolate; width: 50%;',
+        tooltip:'paragraph 1',
+        username:'',
+        password:'',
+        birth:'',
+        city:'', 
+        gender:'',
+        color:[],
+        visibility:'',
+        number: 5,
+        num:'',
+        num2:'',
+        result:'',
+    },
+    // computed melakukan update value otomatis, sedangkan method tidak
+    computed: {
+        sum () {
+            return this.number + 5
+        },
+
+        // multiply(){
+        //     return this.result = parseInt(this.num) * parseInt(this.num2)
+        // }
+    },
+    methods: {
+        multiply(){
+            return this.result = parseInt(this.num) * parseInt(this.num2)
+        }
+    },
 })
