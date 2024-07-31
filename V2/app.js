393a7f6eab217f612b2fcb59fb263ceaf9fc201b
gloriaza <gloriazalawrance@gmail.com>
2024-07-31 14:50:23 +0700

    learn watch property

--- V2/app.js
+++ V2/app.js
@@ -1,39 +1,48 @@
-const app = new Vue ({
-    el:'#app',
-    data:{
-        tampilkan: true,
-        pesan1: 'Selamat datang!',
-        pesan2: 'Selamat tinggal!',
-        gambar: 'image.jpg',
-        link:'https://www.google.com',
-        warna: 'hijau',
-        stylingLink : 'text-decoration: none; color: chocolate; width: 50%;',
-        tooltip:'paragraph 1',
-        username:'',
-        password:'',
-        birth:'',
-        city:'', 
-        gender:'',
-        color:[],
-        visibility:'',
-        number: 5,
-        num:'',
-        num2:'',
-        result:'',
+const app = new Vue({
+  el: "#app",
+  data: {
+    tampilkan: true,
+    pesan1: "Selamat datang!",
+    pesan2: "Selamat tinggal!",
+    gambar: "image.jpg",
+    link: "https://www.google.com",
+    warna: "hijau",
+    stylingLink: "text-decoration: none; color: chocolate; width: 50%;",
+    tooltip: "paragraph 1",
+    username: "",
+    password: "",
+    birth: "",
+    city: "",
+    gender: "",
+    color: [],
+    visibility: "",
+    number: 5,
+    num: "",
+    num2: "",
+    result: "",
+    watchName: "",
+    watchResult: "",
+  },
+  watch: {
+    watchName: "waiting",
+  },
+  // computed melakukan update value otomatis, sedangkan method tidak
+  computed: {
+    sum() {
+      return this.number + 5;
     },
-    // computed melakukan update value otomatis, sedangkan method tidak
-    computed: {
-        sum () {
-            return this.number + 5
-        },
-
-        // multiply(){
-        //     return this.result = parseInt(this.num) * parseInt(this.num2)
-        // }
+    // multiply(){
+    //     return this.result = parseInt(this.num) * parseInt(this.num2)
+    // }
+  },
+  methods: {
+    multiply() {
+      return (this.result = parseInt(this.num) * parseInt(this.num2));
     },
-    methods: {
-        multiply(){
-            return this.result = parseInt(this.num) * parseInt(this.num2)
-        }
+    waiting() {
+      this.watchName.endsWith(".")
+        ? (this.watchResult = `My name is ${this.watchName}`)
+        : (this.watchResult = "Waiting...");
     },
-})
+  },
+});
